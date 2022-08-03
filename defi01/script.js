360a8f58d7924bb1172666b44686d606350c1029
Vitor Oliveira <109984828+TirrCO@users.noreply.github.com>
2022-08-03 11:51:32 -0300

    exercicio maneiraço resolvido

---
+++ defi01/script.js
@@ -0,0 +1,19 @@
+function carregar() {
+    var txt = window.document.getElementById('txt')
+    var img = window.document.getElementById('mini')
+
+    var data = new Date()
+    var hora = data.getHours()
+    txt.innerHTML = `Agora são ${hora}h.`
+
+    if (hora >= 0 && hora < 12) {
+        img.src = 'imagens/manha.png'
+        document.body.style.backgroundColor = '#FC9B2C'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'imagens/tarde.png'
+        document.body.style.backgroundColor = 'lightskyblue'
+    } else {
+        img.src = 'imagens/noite.png'
+        document.body.style.backgroundColor = 'slategray'
+    }
+}
