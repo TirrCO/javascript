be820cc2c16e7452e164dc52ffd1d8769b649bc2
Vitor Oliveira <109984828+TirrCO@users.noreply.github.com>
2022-08-05 11:45:40 -0300

    funções

---
+++ aula16/funcs.js
@@ -0,0 +1,25 @@
+/*
+function parimp(n) {
+    if (n % 2 == 0) {
+        res = 'É par!'
+        return res
+    } else {
+        res = 'É ímpar!'
+        return res
+    }
+}
+
+parimp(6)
+console.log(res)
+*/
+
+function parimp(n) {
+    if (n%2==0) {
+        return 'É par!'
+    } else {
+        return 'É ímpar!'
+    }
+}
+
+let resp = parimp(12)
+console.log(resp)
